63fcec6f19c989819f487aed102939d0fa071fb0
Joton Sutradhar <joton@flyghuri.com>
2026-06-04 11:37:40 +0600

    update

--- src/components/ui/Toaster.tsx
+++ src/components/ui/Toaster.tsx
@@ -5,7 +5,7 @@ import { clsx } from 'clsx'
 
 const styles: Record<ToastType, string> = {
   success: 'bg-green-600 text-white',
-  error:   'bg-amber-600 text-white',
+  error:   'bg-red-600 text-white',
   info:    'bg-blue-600 text-white',
   warning: 'bg-amber-500 text-white',
 }
