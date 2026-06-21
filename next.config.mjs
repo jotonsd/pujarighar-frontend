d4a899b55323949d1ad9ba5ba706a326eb7e81eb
Joton Sutradhar <joton@flyghuri.com>
2026-06-21 23:35:02 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
-import createNextIntlPlugin from 'next-intl/plugin'
+import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin('./src/lib/i18n.ts')
+const withNextIntl = createNextIntlPlugin("src/lib/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
-      { protocol: 'http',  hostname: 'localhost', port: '8020' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: "http", hostname: "localhost", port: "8020" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "dev-api.pujarighar.com" },
     ],
   },
-}
+};
 
-export default withNextIntl(nextConfig)
+export default withNextIntl(nextConfig);
