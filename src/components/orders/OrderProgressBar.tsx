cf3d38bf2a4c9d0ef8af54f40f87fb9b1a211015
Joton Sutradhar <joton@flyghuri.com>
2026-09-14 13:41:51 +0600

    update

--- src/components/orders/OrderProgressBar.tsx
+++ src/components/orders/OrderProgressBar.tsx
@@ -15,9 +15,9 @@ function getStages(isCourier: boolean): { statuses: OrderStatus[]; label_bn: str
     isCourier
       ? { statuses: ["ASSIGNED"], label_bn: "কুরিয়ারে পাঠানো হয়েছে", label_en: "Sent to Courier" }
       : { statuses: ["ASSIGNED"], label_bn: "এসাইন্ড", label_en: "Assigned" },
-    { statuses: ["PICKED"], label_bn: "পিকআপ হয়েছে", label_en: "Picked Up" },
+    { statuses: ["PICKED"], label_bn: "পিকডআপ", label_en: "Picked Up" },
     { statuses: ["ON_THE_WAY"], label_bn: "পথে আছে", label_en: "In Transit" },
-    { statuses: ["DELIVERED"], label_bn: "ডেলিভারি হয়েছে", label_en: "Delivered" },
+    { statuses: ["DELIVERED"], label_bn: "ডেলিভারড", label_en: "Delivered" },
   ];
 }
 
